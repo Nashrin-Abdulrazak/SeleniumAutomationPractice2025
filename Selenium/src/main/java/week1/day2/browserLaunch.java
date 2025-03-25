5f21fe93b52346c161ce7bdf736bdbad07232cba
umarm <umarm@IMRAN_NASRIN>
2025-03-25 16:37:51 +0530

    comment updated for selenium package

--- Selenium/src/main/java/week1/day2/browserLaunch.java
+++ Selenium/src/main/java/week1/day2/browserLaunch.java
@@ -6,6 +6,13 @@ public class browserLaunch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+
+		// TODO Auto-generated method stub
+
+		// TODO Auto-generated method stub
+
+		
 ChromeDriver driver=new ChromeDriver();
 driver.manage().window().maximize();
 driver.get("https://mvnrepository.com/search?q=selenium");
